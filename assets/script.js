209751a536e04cccf4f87c61b2a8aa631d3f19e2
cfprice3 <cfprice3@gmail.com>
2020-03-02 14:40:04 -0500

    found how to generate the date in js, added a today variable for currenet day.

--- assets/script.js
+++ assets/script.js
@@ -4,4 +4,5 @@ console.log(cityName)
 var queryURL1 = "api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + apiKey;
 var latLon = "";
 var queryURL2 = "http://api.openweathermap.org/data/2.5/uvi?appid={appid}&lat={lat}&lon={lon}";
+var today = new Date();
 
