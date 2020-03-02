c5dbda62cd9698607f667db6cfbf31b6621e83be
cfprice3 <cfprice3@gmail.com>
2020-03-02 01:19:19 -0500

    started creating and defining variables on JS page.  added var for api key, and for getURL to pull from apis

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+var apiKey = "74193b84a905b258e148fc4d98fd703e";
+var cityName = $("#cityInput").val();
+console.log(cityName)
+var queryURL1 = "api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + apiKey;
+var latLon = "";
+var queryURL2 = "http://api.openweathermap.org/data/2.5/uvi?appid={appid}&lat={lat}&lon={lon}";
+
